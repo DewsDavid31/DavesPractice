8a3db093d2a34b7daba7db492e57ae88e0c69006
Dews <david.dews@dish.com>
2022-09-01 11:28:55 -0600

    rusty on pointers, need to figure out arrays

--- InProgress/BasicBigDataStruct.c
+++ InProgress/BasicBigDataStruct.c
@@ -1,22 +1,12 @@
-import <stdio.h>
-import <string.h>
+#include <stdio.h>
+int main(){
+	printf("My pointers are rusty, so I need some work before merging trees");
+	printf("unsorted array 1,5,3,2,5,76\n");
+	int testArray[] = {1,5,3,2,5,76};
+	for(int x = 0; x < sizeof(testArray)/sizeof(testArray[0]); x = x + 1){
+		printf("%i,", testArray[x]);
+	}
+}
 
-struct DiskNode{
-/*Heap only*/
-}
-struct MemNode{
-/*Stack only*/
-}
-struct DiskBTree{
-	DiskNode[] nodes;
-	DiskNode root;	
 
-}
-struct MemBTree{
-	MemNode[] nodes;
-	MemNode root;	
-}
-struct LSMTree{
-	MemBTree c0;
-	DiskBTree c1;
-}
+
