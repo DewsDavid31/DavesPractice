6995ec8b543af50acdf39453540b61c2be51ae97
Dews <david.dews@dish.com>
2022-09-01 13:13:55 -0600

    added notes on making basic cloud during break

--- InProgress/BasicCloud.java
+++ InProgress/BasicCloud.java
@@ -7,6 +7,15 @@ private class LoadBalancer{
 }
 
 private class PrivateKey{
+	public int rsa(string data){
+		// pick two prime numbers p,q
+		// compute n =p*q
+		// find totient phi(n) = (p-1)(q-1)
+                // choose a d that is prime in Z such that 1<e<phi(n), e is coprime to phi(n), gcd(e,phi(n)) = 1
+		// and find e^n such that e*dmod(p-1)(q-1) = 1
+		// decrypt D= c^d(modn), encrypt C = D^e(modn)
+	}
+	
 }
 
 private class PublicKey{
