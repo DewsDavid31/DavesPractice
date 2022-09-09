4a8fefefe7fbb3fa9aaba7c97ed514c07e5a705e
David Dews <davidjdews@gmail.com>
2022-09-09 12:23:58 -0600

    Encryption functional, inverse mod be damned

--- InProgress/BasicEncryption.py
+++ InProgress/BasicEncryption.py
@@ -11,46 +11,51 @@ class bad_rsa_key:
   
   def find_p_q(self,e):
      found = False
-     print("Calculating p,q")
      while(not found):
-       p = self.pick_rand_prime(1000)
-       q = self.pick_rand_prime(1000)
+       p = self.pick_rand_prime(e,100)
+       q = self.pick_rand_prime(e,100)
        if self.gcd(e,(p-1)*(q-1)) == 1:
          print("found: p=" + str(p) + " q=" + str(q) + " e=" + str(e))
          found = True
      return p,q
     
+  def inverse_mod_brute_force(self, a,b):
+    index = 1
+    while(a*index % b != 1):
+      index = index + 1
+    return index 
+      
   def find_d(self,p,q):
     print("Calculating d")
-    d = (self.e ** -1 % (p-1)*(q-1))
+    d = self.inverse_mod_brute_force(self.e,(p-1)*(q-1))
     print("Found d=" + str(d))
     return d
     
-  def pick_rand_prime(self, cap):
+  def pick_rand_prime(self,start, cap):
     prime = False
     while(not prime):
-      candidate = random.randint(3,cap);
+      candidate = random.randint(start,cap);
       prime = True
       for factors in range(2,candidate):
         if candidate % factors == 0:
           prime = False
     return candidate
-    
+
+
   def __init__(self):
-      self.e = self.pick_rand_prime(1000)
+      self.e = self.pick_rand_prime(2,100)
       self.p,self.q = self.find_p_q(self.e)
       self.d = self.find_d(self.p, self.q)
 
   def encrypt_char(self, datum):
     n = self.p * self.q
-    print("value of " + str(datum) + " is: " + str(ord(datum)))
-    print("Becomes: " + str(math.ceil(((ord(datum) ** self.e) % n))))
     return chr(math.ceil(((ord(datum) ** self.e) % n)))
 
   def decrypt_char(self, datum):
     n = self.p * self.q
-    print("value of " + str(datum) + " is: " + str(ord(datum)))
-    print("Becomes: " + str(math.ceil((ord(datum) ** self.d) % (n))))
+    converted_num = math.ceil(((ord(datum) ** self.d) % n))
+    converted = chr(converted_num)
+    print(str(datum) + " is " + str(ord(datum))+ " Decrypts to: " + str(converted_num) + " or " + converted)
     return chr(math.ceil(((ord(datum) ** self.d) % n)))
     
 if __name__ == "__main__":
