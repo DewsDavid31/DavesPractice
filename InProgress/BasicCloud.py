138a9ecc131bcfa07832ddaebfdf02e0f1a7d43b
David Dews <davidjdews@gmail.com>
2022-09-09 09:56:12 -0600

    Encryption works, decryption doesn't

--- InProgress/BasicCloud.py
+++ InProgress/BasicCloud.py
@@ -9,19 +9,21 @@ class bad_rsa_key:
     else:
       return self.gcd(b, a%b)
   
-  def find_d_e(self,x,n,p,q):
+  def find_e(self,p,q):
      # choose a d that is prime in Z 
      found = False
+     print("Calculating public key")
      while(not found):
-       d = self.pick_rand_prime(x)
-       e = self.pick_rand_prime(x)
-       if self.gcd(e,x) == 1 and (e*d % ((p-1)*(q-1))) == 1:
+       e = self.pick_rand_prime(1000)
+       if self.gcd(e,(p-1)*(q-1)) == 1:
          found = True
-        # 1<e<phi(n)
-        # e is coprime to phi(n), gcd(e,phi(n)) = 1
-        # and find e^n such that e*dmod(p-1)(q-1) = 1
-     return (d,e)
-   
+     return e
+
+  def find_d(self,p,q):
+    print("Calculating private key")
+    d = (self.e ** -1) % ((p-1)*(q-1)) 
+    return d
+    
   def pick_rand_prime(self, cap):
     prime = False
     while(not prime):
@@ -31,29 +33,20 @@ class bad_rsa_key:
         if candidate % factors == 0:
           prime = False
     return candidate
-
-  def linear_totient(self,p,q):
-    # find totient phi(n) = (p-1)(q-1)
-    n = p * q
-    factor_1 = p - 1
-    factor_2 = q - 1
-    second_n = factor_1*factor_2
-    new_slope = abs(second_n - n)
-    return new_slope
-
-  # decrypt D= c^d(modn), encrypt C = D^e(modn)
+    
   def __init__(self):
-      p = self.pick_rand_prime(10)
-      q = self.pick_rand_prime(10)
-      n  = p*q
-      x = self.linear_totient(p,q)
-      self.d,self.e = self.find_d_e(x,n,p,q)
+      p = self.pick_rand_prime(1000)
+      q = self.pick_rand_prime(1000)
+      self.e = self.find_e(p,q)
+      self.d = self.find_d(p,q)
+      self.p = p
+      self.q = q
 
-  def encrypt_char(self, datum, n):
-    return chr((ord(datum) ** self.e) % n)
+  def encrypt_char(self, datum):
+    return chr(math.ceil((ord(datum) ** (self.e) % (self.p * self.q))))
 
-  def decrypt_char(self, datum , n):
-     return chr((ord(datum) ** self.d) % n)
+  def decrypt_char(self, datum):
+    return chr(math.ceil(ord(datum) ** ((self.d)) % (self.p * self.q)))
     
 if __name__ == "__main__":
   test_key = bad_rsa_key()
@@ -61,13 +54,14 @@ if __name__ == "__main__":
   print("Encrypting " + test_string + "...")
   output_str = ""
   for charac in test_string:
-    output_str += test_key.encrypt_char(charac,98) + " "
+    print(charac + test_key.encrypt_char(charac))
+    output_str += test_key.encrypt_char(charac) + " "
   print("Encyrpted badly: " + output_str)
-  output_chars = output_str.split()
+  output_chars = output_str.split(' ')
   reverted_str= ""
   for chars in output_chars:
     if chars == '':
-      break
-    reverted_str += str(test_key.decrypt_char(chars,98))
+      continue
+    reverted_str += str(test_key.decrypt_char(chars))
   print("Decrypted badly: " + reverted_str)
     
