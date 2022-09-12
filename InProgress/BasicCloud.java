5bc0aa85a047438eb813062fa9ed3aca3238f0fa
David Dews <davidjdews@gmail.com>
2022-09-12 11:49:48 -0600

    Encryption converging to zero, did something wrong

--- InProgress/BasicCloud.java
+++ InProgress/BasicCloud.java
@@ -1,57 +1,152 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Math.*;
+
+class BadKey {
+  public Random gen;
+  private int e;
+  private int q;
+  private int d;
+  private int p;
+  public int limit;
+  
+  public BadKey(int limit) {
+    this.gen = new Random();
+    this.limit = limit;
+    this.e = pickPrime(2,limit);
+    int[] pnq = this.find_p_q();
+    this.p = pnq[0];
+    this.q = pnq[1];
+    this.d = findD();
+  }
+  
+  public int pickPrime(int start, int limit){
+    int candidate = 2;
+    boolean prime = false;
+    while(!prime){
+      candidate = gen.nextInt(limit);
+      prime = true;
+      if(candidate < start){
+        prime = false;
+        continue;
+      }
+      for(int check = 2; check < candidate; check++){
+        if(candidate % check == 0){
+          prime = false;
+        }
+      }
+    }
+    return candidate;
+  }
+  
+  public int gcd(int a, int b){
+    if(b == 0){
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+  
+  public int inverseModBrute(int a, int b) {
+    int index = 0;
+    while (a * index % b != 1) {
+      index++;
+    }
+    return index;
+  }
+
+
+  public int[] find_p_q(){
+    boolean found = false;
+    while(!found){
+       p = this.pickPrime(2,e);
+       q = this.pickPrime(2,e);
+       int totient = (p-1)*(q-1);
+       if(this.gcd(e, totient) == 1){
+         found = true;
+       }
+    }
+    int[] results = {p,q};
+    return results;
+  }
+
+  public int findD(){
+    int totient = (p-1)*(q-1);
+    return inverseModBrute(e, totient);
+  }
+  
+  public char encrypt(char data){
+    int n = p * q;
+    int numeric = (int) data;
+    System.out.println(Character.toString(data) + numeric);
+    int resultNum = (int) Math.ceil(Math.pow(numeric,e) % n);
+    char result = (char) resultNum;
+    System.out.println(Character.toString(result) + resultNum);
+    return result;
+  }
+
+  
+  public char decrypt(char data){
+    int n = p * q;
+    int numeric = (int) data;
+    int resultNum = (int) Math.ceil(Math.pow(numeric,d) % n);
+    char result = (char) resultNum;
+    return result;
+  }
+}
+
+class User {
+  private BadKey key1;
+
+  public User() {
+    this.key1 = new BadKey(10000);
+  }
+
+  public String encrypt(String data) {
+    String result = "";
+    System.out.println("Encrypting \"" + data + "\" for uploading...");
+    for (int index = 0; index < data.length(); index++) {
+      char next = data.charAt(index);
+      char nextResult = key1.encrypt(next);
+      result += Character.toString(nextResult);
+    }
+    System.out.println("Converted to: " + result);
+    return result;
+  }
+
+  public String decrypt(String data) {
+    String result = "";
+    for (int index = 0; index < data.length(); index++) {
+      char next = data.charAt(index);
+      char nextResult = key1.decrypt(next);
+      result += Character.toString(nextResult);
+    }
+    return result;
+  }
+}
+
 class Cloud {
-  String[][] hardDrives;
-  int[][] jumpTable;
-  int[][] deviceTable;
-  int[][] Authorizations;
-  String[][] uploads;
-  final int BLOCKSIZE = 40;
+  public ArrayList<String> uploads;
 
-  public void Cloud(){
-    this.jumpTable = new int[2][4];
-    this.uploads = new String[2][400];
-    this.deviceTable = new int[2][4];
-    this.hardDrives = new String[2][400];
+  public Cloud() {
+    this.uploads = new ArrayList<String>();
   }
 
-  public void loadBalanceRoundRobin() {
-    int userIndex = 0;
-    int deviceIndex = 0;
-    for (int uploadIndex = 0; uploadIndex < this.uploads[0].length; uploadIndex++) {
-      this.hardDrives[deviceIndex][uploadIndex] = this.uploads[userIndex][uploadIndex];
-      userIndex++;
-      deviceIndex++;
-      userIndex = userIndex % 2;
-      deviceIndex = deviceIndex % 2;
-    }
+  public String read(User user, int index) {
+    String unparsed = uploads.get(index);
+    return user.decrypt(unparsed);
+
   }
 
-  public void upload(int user, String data) {
-    int blockIndex = 0;
-    for (int charIndex = 0; charIndex < data.length(); charIndex += BLOCKSIZE) {
-      this.uploads[user][blockIndex] = data.substring(blockIndex * BLOCKSIZE, charIndex);
-    }
+  public void upload(User user, String data) {
+    uploads.add(user.encrypt(data));
   }
-
-  public String read(int device, int location) {
-    String result = hardDrives[device][location];
-    if (this.jumpTable[device][location] != -1) {
-      return result + this.read(deviceTable[device][location], jumpTable[device][location]);
-    } else {
-      return result;
-    }
-  }
-
 }
 
 class Main {
   public static void main(String[] args) {
     Cloud testCloud = new Cloud();
-    testCloud.upload(0, "I can see this!");
-    testCloud.upload(1, "I am not allowed to see this");
-    testCloud.upload(0, "this file is too large to be only on my pc !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    System.out.println(testCloud.read(0,1));
-    System.out.println(testCloud.read(0,2));
-    System.out.println(testCloud.read(0,3));
-    System.out.println(testCloud.read(0,4));
+    User me = new User();
+    testCloud.upload(me, "I can see this!");
+    System.out.println(testCloud.read(me, 0));
   }
 }
