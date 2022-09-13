dad27b36cb34d42ff22da95c3e9560ac45514e0b
David Dews <davidjdews@gmail.com>
2022-09-13 09:30:48 -0600

    Finally functional at encrypting users data
    Thank god for BigInteger!

--- InProgress/BasicCloud.java
+++ InProgress/BasicCloud.java
@@ -1,51 +1,51 @@
-import java.util.ArrayList;
+  import java.util.ArrayList;
 import java.util.Random;
-import java.lang.Math;
+import java.math.BigInteger;
 
 class BadKey {
   public Random gen;
+  private int n;
+  private int totient;
   private int e;
   private int q;
   private int d;
   private int p;
   public int limit;
-  
+
   public BadKey(int limit) {
     this.gen = new Random();
     this.limit = limit;
     this.e = this.pickPrime(3, limit);
-    System.out.println("Picked: " + e);
     this.find_p_q();
     this.findD();
-    System.out.println("Private: "+ this.p + ", " + this.q + " public: " + this.d + ", " + this.e);
   }
-  
-  public int pickPrime(int start, int limit){
+
+  public int pickPrime(int start, int limit) {
     int candidate = 2;
     boolean prime = false;
-    while(!prime){
+    while (!prime) {
       candidate = gen.nextInt((int) limit - 2) + 2;
       prime = true;
-      if(candidate < start || candidate > limit){
+      if (candidate < start || candidate > limit) {
         prime = false;
         continue;
       }
-      for(int check = 2; check < candidate; check++){
-        if(candidate % check == 0){
+      for (int check = 2; check < candidate; check++) {
+        if (candidate % check == 0) {
           prime = false;
         }
       }
     }
     return candidate;
   }
-  
-  public int gcd(int a, int b){
-    if(b == 0){
+
+  public int gcd(int a, int b) {
+    if (b == 0) {
       return a;
     }
     return gcd(b, a % b);
   }
-  
+
   public int inverseModBrute(int a, int b) {
     int index = 1;
     while (a * index % b != 1) {
@@ -54,54 +54,53 @@ class BadKey {
     return index;
   }
 
-
-  public void find_p_q(){
+  public void find_p_q() {
     boolean found = false;
     System.out.println("Generating public key...");
-    while(!found){
-       this.p = this.pickPrime(this.e + 1,this.limit);
-       this.q = this.pickPrime(this.e + 1,this.limit);
-       int totient = (p-1)*(q-1);
-       if(this.gcd(e, totient) == 1){
-         found = true;
-       }
+    while (!found) {
+      this.p = this.pickPrime(this.e + 1, this.limit);
+      this.q = this.pickPrime(this.e + 1, this.limit);
+      this.totient = (p - 1) * (q - 1);
+      if (this.gcd(this.e, this.totient) == 1) {
+        found = true;
+        this.n = this.p * this.q;
+      }
     }
   }
 
-  public void findD(){
+  public void findD() {
     System.out.println("Generating private key...");
-    int totient = (p-1)*(q-1);
-    this.d = inverseModBrute(this.e, totient);
+    this.d = inverseModBrute(this.e, this.totient);
   }
-  
-  public char encrypt(char data){
-    int n = this.p * this.q;
-    int numeric = (int) data;
-    long resultNum = (int) (Math.ceil(Math.pow(numeric, (double) this.e)) % n);
-    char result = (char) resultNum;
+
+  public char encrypt(char data) {
+    BigInteger numeric = BigInteger.valueOf((int) data);
+    BigInteger resultNum = numeric.modPow(BigInteger.valueOf(this.e),BigInteger.valueOf(this.n));
+    char result = (char) resultNum.intValue();
     return result;
   }
 
-  
-  public char decrypt(char data){
+  public char decrypt(char data) {
     int n = this.p * this.q;
-    int numeric = (int) data;
-    int resultNum = (int) (Math.ceil(Math.pow(numeric, (double) this.d)) % n);
-    char result = (char) resultNum;
+    BigInteger numeric = BigInteger.valueOf(data);
+    BigInteger resultNum = numeric.modPow(BigInteger.valueOf(this.d),BigInteger.valueOf(n));
+    char result = (char) resultNum.intValue();
     return result;
   }
 }
 
 class User {
   private BadKey key1;
-
-  public User(int cap) {
+  public String name;
+  
+  public User(String name, int cap) {
     this.key1 = new BadKey(cap);
+    this.name = name;
   }
 
   public String encrypt(String data) {
     String result = "";
-    System.out.println("Encrypting \"" + data + "\" for uploading...");
+    System.out.println(this.name + " is Encrypting \"" + data + "\" for uploading...");
     for (int index = 0; index < data.length(); index++) {
       char next = data.charAt(index);
       char nextResult = this.key1.encrypt(next);
@@ -113,7 +112,7 @@ class User {
 
   public String decrypt(String data) {
     String result = "";
-    System.out.println("Decrypting \"" + data + "\" for reading...");
+    System.out.println(this.name + " is Decrypting \"" + data + "\" for reading...");
     for (int index = 0; index < data.length(); index++) {
       char next = data.charAt(index);
       char nextResult = this.key1.decrypt(next);
@@ -144,8 +143,12 @@ class Cloud {
 class Main {
   public static void main(String[] args) {
     Cloud testCloud = new Cloud();
-    User me = new User(100);
+    User me = new User("me", 100);
+    User notMe = new User("Someone Else!", 100);
     testCloud.upload(me, "I can see this!");
+    testCloud.upload(notMe, "I can\'t see this, this is someone's file on my pc");
     System.out.println(testCloud.read(me, 0));
+    System.out.println("Reading file Im not allowed access to...");
+    System.out.println(testCloud.read(me, 1));
   }
 }
