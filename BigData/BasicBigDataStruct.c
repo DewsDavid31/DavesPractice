5e4cce40732622cb895eda5ae112cae0e2b182a9
David Dews <davidjdews@gmail.com>
2022-09-19 12:09:53 -0600

    Functional demo, even though not on disk yet.

--- BigData/BasicBigDataStruct.c
+++ BigData/BasicBigDataStruct.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-struct SSTable{
+struct LSM{
    int* segtable;
    int* tree;
    int* treedepth;
    int* currentSeg;
-} sstable;
+   int* indextable;
+   int* indextablesize;
+} lsm;
 
 
 int* btreeify(int* arrayptr, int size){
@@ -155,81 +157,180 @@ int* mergesort(int* arrayptr, int size){
     }
 }
 
-void compaction(struct SSTable table){
+void compaction(struct LSM table, int size){
   printf("\nCompacting sstable...");
   for(int index = 0; index < table.currentSeg[0]; index++){
      for(int index1 = index + 1; index1 < table.currentSeg[0]; index1++){
        if(table.segtable[index1] == table.segtable[index] && table.segtable[index] != 0){
          table.segtable[index] = 0;
        }
-     }  
-  }
-  
+     }
   int* newptr = malloc(table.currentSeg[0] * sizeof(int));
   newptr = mergesort(table.segtable, table.currentSeg[0]);
   for(int swapindex = 0; swapindex < table.currentSeg[0]; swapindex++){
     table.segtable[swapindex] = newptr[swapindex];
   }
+ }
+  while(table.segtable[table.currentSeg[0]] == 0){  
+     table.currentSeg[0]--;  
+  }
+  int current = table.currentSeg[0];
+  if(current < size){
+    table.currentSeg[0] = size + 1;
+  }
+  else{
+    table.currentSeg[0] += (current % size) + 1;
+  }
 }
 
-void showSSTable(struct SSTable table, int size){
-  printf("\ntree:\n");
+void showLSM(struct LSM table, int arraysize){
+  int size = arraysize * 2;
+  printf("\ntree: ");
   for(int index1 = 0; index1 < table.treedepth[0]; index1++){
       printf("%d,", table.tree[index1]);
   }
-  printf("\nsstable:\n");
+  printf("\n\nsstable:\n");
 
   for(int index = 0; index < table.currentSeg[0]; index++){
     if(index % size == 0){
-      printf("\nsegment %d: ", index / size);
+      printf("\n\tsegment %d: ", index / size);
     }
     printf("%d, ",table.segtable[index]);
   }
+  printf("\n\n");
 }
 
-void flush(int* treein, int size, struct SSTable table){
-  int* sorted = mergesort(treein, size);
+void flush( int size, struct LSM table){
+  int* unsorted = malloc(size * sizeof(int));
+  for(int index = 0; index < size; index++){
+    unsorted[index] = table.tree[index];
+  }
+  int* sorted = mergesort(unsorted, size);
   for(int arrayindex = 0; arrayindex < size; arrayindex++){
       if(sorted[arrayindex] != 0){
-         table.segtable[arrayindex + table.currentSeg[0]] = sorted[arrayindex];
+         table.segtable[arrayindex + table.currentSeg[0] - 1] = sorted[arrayindex];
       }
     }
-  printf("\n");
-  for(int arrayindex2 = 0; arrayindex2 < size; arrayindex2++){
-      printf("%d ,", table.segtable[arrayindex2]);
-    }
-  printf("\n");
-  printf("Flushing b-tree into sstable...\n");
+  for(int clear = 0; clear < size; clear++ ){
+    table.tree[clear] = 0;
+  }
+  printf("\nFlushing b-tree into sstable...\n");
   table.currentSeg[0]+=size;
+  table.indextable[table.indextablesize[0]] = table.segtable[table.currentSeg[0]];
+  table.indextablesize[0]++;
 }
 
-void addTree(int* array, int arraysize, struct SSTable table){
+void addTree(int* array, int arraysize, struct LSM table){
   int* tree = btreeify(array, arraysize);
-  table.tree = &tree;
+  for(int index = 0; index < arraysize * 2; index++){
+    table.tree[index] = tree[index];
+  }
+  table.tree = tree;
   table.treedepth[0] = arraysize * 2;
 }
 
-struct SSTable createLSM(int arraysize, int* array){
+struct LSM createLSM(int arraysize, int* array){
   int treesize = arraysize * 2;
-  struct SSTable testTable;
+  struct LSM testTable;
   int* segs = malloc(arraysize * 200 * sizeof(int));
   testTable.segtable = segs;
   testTable.currentSeg = malloc(sizeof(int));
   testTable.currentSeg[0] = 1;
   testTable.treedepth = malloc(sizeof(int));
-  testTable.treedepth[0] = 1;
+  testTable.treedepth[0] = 0;
+  testTable.tree = malloc(arraysize * 2 * sizeof(int));
+  testTable.indextable = malloc(200 * sizeof(int));
+  testTable.indextablesize = malloc(sizeof(int));
+  testTable.indextablesize[0]=1;
   addTree(array, arraysize, testTable);
-  int* ptr = malloc(arraysize * sizeof(int));
-  for(int clear = 0; clear < treesize; clear++){
-    testTable.segtable[clear] = 0;
-  }
   return testTable;
 }
 
+int searchTree(int root, int key, struct LSM lsm){
+  if(root >= lsm.treedepth[0]){
+    return -1;
+  }
+  else if(key == lsm.tree[root]){
+    return root;
+  }
+  else if(key > lsm.tree[root]){
+    return searchTree((root * 2) + 2, key, lsm);
+  }
+  else{
+    return searchTree((root * 2) + 1, key, lsm);
+  }
+}
+
+int read(int key, struct LSM lsm, int arraysize){
+  int result = -1;
+  int treeresult = searchTree(0, key, lsm);
+  if(treeresult != -1){
+    printf("Found in memory tree!");
+    return treeresult;
+  }
+  else{
+    int start = 0;
+    for(int segindex = 0; segindex < lsm.indextablesize[0]; segindex++){
+      if(lsm.indextable[segindex] >= key){
+        start = segindex * arraysize;
+      }
+    }
+    for(int remainder = start; remainder < lsm.currentSeg[0]; remainder++){
+      if(lsm.segtable[remainder] == key){
+        printf("Found in storage segments!");
+        return remainder;
+      }
+    }
+    return result;
+  }
+}
+
+void menu(int selection, struct LSM lsm, int arraysize){
+  if(selection == 0){
+    showLSM(lsm, arraysize);
+    printf("LSM object demo:\n1. Write a tree of size %d to LSM\n2. Read a value from lsm\n3. Flush memory to disk\n4. compress/compact segments\n5. Quit\n", arraysize);
+  int userselection = 0;
+  printf("Make a selection: ");
+  scanf("%d", &userselection);
+  printf("\n");
+  menu(userselection, lsm, arraysize);
+  }
+  else if(selection == 1){
+    int* newarray = malloc(arraysize * sizeof(int));
+    for(int inputindex = 0; inputindex < arraysize; inputindex++){
+      printf("\nEnter a value: ");
+      scanf("%d", &newarray[inputindex]);
+    }
+    addTree(newarray, arraysize, lsm);
+    menu(0,lsm,arraysize);
+  }
+  else if(selection == 2){
+    int key = 0;
+    int source = 0;
+    printf("\nEnter a value to find: ");
+    scanf("%d", &key);
+    int result = read(key, lsm, arraysize);
+    if(result == -1){
+      printf("\nNot found in LSM object!\n");
+    }
+    else{
+      printf("\nFound in index: %d with value: %d\n", result, key);
+    }
+    menu(0,lsm,arraysize);
+  }
+  else if(selection == 3){
+    flush(arraysize * 2, lsm);
+    menu(0,lsm,arraysize);
+  }
+  else if(selection == 4){
+    compaction(lsm, arraysize * 2);
+    menu(0, lsm, arraysize);
+  }
+}
+
 int main(){
-  int* array = malloc(6 * sizeof(int));
-  struct SSTable lsm = createLSM(6, array);
-  showSSTable(lsm, 12);
-  compaction(lsm);
-  showSSTable(lsm, 12);
+  int arraysize = 6;
+  int* array = malloc(arraysize * sizeof(int));
+  struct LSM lsm = createLSM(arraysize, array);
+  menu(0, lsm, arraysize);
 }
